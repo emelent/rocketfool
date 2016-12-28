a596aa0256351c94379ad06de59cad562d08c0ec
Merrick Kavolsky <technicmk@gmail.com>
2016-12-28 22:14:21 +0200

    Complete DropDownSearch component

---
+++ src/components/DropDownSearch.js
@@ -0,0 +1,193 @@
+import React from 'react';
+import Radium from 'radium';
+
+import SearchBox from './SearchBox';
+
+const styles = {
+  container: {
+    display: 'inline-block',
+    height: '60px',
+    minWidth: '280px',
+    maxWidth: '70%',
+    width: '280px',
+    transition: 'all 0.4s cubic-bezier(.17,.67,.17,.88)',
+  },
+
+  searchBox: {
+    display: 'block',
+    width: 'auto',
+    height: '60px',
+    overflow: 'hidden',
+    marginRight: '80px',
+  },
+  iconContainer: {
+    float: 'right',
+    background: 'rgba(255,255,255,0.8)',
+    display: 'inline-block',
+    height: '60px',
+    width: '80px',
+    lineHeight: '60px',
+    padding: '0 30px',
+    color: '#555',
+    cursor: 'pointer',
+  },
+  icon: {
+    fontWeight: 'bold',
+    transition: 'all 0.4s cubic-bezier(.17,.67,.17,.88)',
+  },
+  icon__open: {
+    transform: 'rotateZ(180deg)',
+  },
+
+  list:{
+    maxHeight: '300px', 
+    overflowY: 'scroll',
+    background: '#fff',
+    color: '#555',
+    textAlign: 'left',
+    transformOrigin: 'top',
+    transform: 'scaleY(0)',
+    transition: 'all 0.2s cubic-bezier(.17,.67,.17,.88)',
+  },
+
+  list__open:{
+    transform: 'scaleY(1)',
+  },
+
+  listItem:{
+    padding: '20px',
+    cursor: 'pointer',
+
+    ':hover':{
+      background: '#eee',
+    }
+  },
+
+  listItem__active:{
+    color: '#673AB7',
+    fontWeight: 'bold',
+    cursor: 'auto',
+    ':hover':{
+      background: '#fff',
+    }
+  },
+
+};
+
+@Radium
+class DropDownSearch extends React.Component{
+  
+  static propTypes = {
+    items: React.PropTypes.array.isRequired,
+    activeIndex: React.PropTypes.number,
+    onItemClick: React.PropTypes.func,
+    onOpen: React.PropTypes.func,
+    onClose: React.PropTypes.func,
+    onSearchSubmit: React.PropTypes.func,
+  };
+
+  static defaultProps = {
+    activeIndex: 0,
+    onItemClick: () => {},
+    onOpen: () => {},
+    onClose: () => {},
+  };
+
+  constructor(props){
+    super(props);
+    
+    this.__onMenuClick= this.__onMenuClick.bind(this);
+    this.__onOpen = this.__onOpen.bind(this);
+    this.__onClose = this.__onClose.bind(this);
+    this.__onFocus = this.__onFocus.bind(this);
+    this.__onBlur = this.__onBlur.bind(this);
+    this.state = {
+      open: false,
+      focus: false,
+    };
+  }
+
+  __onFocus(){
+    this.setState({focus: true});
+  }
+
+  __onBlur(){
+    this.setState({focus: false});
+  }
+
+  __onMenuClick(){
+    if(this.state.open){
+      this.__onClose();
+    }else{
+      this.__onOpen();
+    }
+  }
+
+  __onOpen(){
+    this.setState({open: true});
+    this.props.onOpen();
+  }
+
+  __onClose(){
+    this.setState({open: false});
+    this.props.onClose();
+  }
+
+  
+  renderItems(items, activeIndex, onItemClick){
+    return items.map((el, i) => {
+      if(i == activeIndex)
+        return null;
+      return (
+        <div style={styles.listItem}
+          key={i}
+          onClick={() => {onItemClick(i);}}
+        >
+          {el}
+        </div>
+      );
+    });
+  }
+
+  render(){
+    let {style, items, onItemClick, activeIndex, onSearchSubmit} = this.props;
+    let {open, focus} = this.state;
+    
+    let ddlStyle = {...styles.list};
+    let iconStyle = {...styles.icon};
+    if(open){
+      iconStyle = {...iconStyle, ...styles.icon__open};
+      ddlStyle = {...ddlStyle, ...styles.list__open};
+    }
+
+    if(focus){
+      style = {...style, width: '400px'} 
+    }
+
+    return (
+      <div style={{...styles.container, ...style}}>
+        <div style={styles.iconContainer}
+          onClick={this.__onMenuClick}
+        >
+          <i className="fa fa-angle-down"
+            style={iconStyle}
+          ></i>
+        </div>
+        <SearchBox style={styles.searchBox} 
+          onSubmit={onSearchSubmit}
+          focusWidth="auto"
+          onFocus={this.__onFocus}
+          onBlur={this.__onBlur}
+        />
+        <div style={ddlStyle}>
+          <div style={{...styles.listItem, ...styles.listItem__active}}>
+            {items[activeIndex]}
+          </div>
+          {this.renderItems(items, activeIndex, onItemClick)}
+        </div>
+      </div>
+    );
+  }
+};
+
+export default DropDownSearch;
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
-import SearchBox from '../components/SearchBox';
-import TabBar from '../components/TabBar';
+//import TabSearch from '../components/TabSearch';
+import DropDownSearch from '../components/DropDownSearch';
 
 
-const tabItems = ['All', 'Some', 'Green Ones', 'Blue Ones'];
+const dropDownItems = ['All', 'Some', 'Green Ones', 'Blue Ones'];
+
+const styles = {
+  container:{
+    background: 'url("/assets/images/header-bg.jpg")',
+    backgroundSize: 'cover',
+    backgroundAttachment: 'fixed',
+    minHeight: '100vh',
+    color: '#fff',
+  },
+
+  searchBox:{
+    margin: 'auto',
+  }
+};
 
 class Header extends React.Component{
   constructor(props){
@@ -20,15 +34,20 @@ class Header extends React.Component{
     console.log('active => ', i);
   }
 
+  __onSearchSubmit(text){
+    alert(`You search '${text}'`);
+  }
+
   render(){
     return (
-      <div>
-        <h2>Header</h2>
-        <SearchBox />
-        <TabBar items={tabItems} 
-          activeIndex={this.state.activeIndex}
-          onItemClick={this.__onTabItemClick}
-        />
+      <div style={styles.container}>
+        <div>Header</div>
+        <div style={{textAlign: 'center'}}>
+          <DropDownSearch style={styles.searchBox}
+            items={dropDownItems}
+            onSearchSubmit={this.__onSearchSubmit}
+          />
+        </div>
       </div>
     );
   }
